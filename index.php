c5e83ea5f0d07538f8a95bf970f0c39ebc1ec513
aggabaorenz16 <43602006+aggabaorenz16@users.noreply.github.com>
2022-10-12 23:25:34 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplication</title>
+</head>
+<body>
+  <div class="form">
+  <?php
+
+  echo '<h1>Multiplication</h1>';
+
+echo '<input type="number" placeholder="Number 1" id="num1">';
+/* echo '<br>'; */
+echo '<input type="number2" placeholder="Number 2" id="num2">';
+/* echo '<br>'; */
+echo '<input type="submit" value="Compute" onClick="compute()">';
+
+echo '<p id="solution">';
+echo '<p id="result">';
+echo '<p id="fnum">';
+
+
+
+?>
+  </div>
+</body>
+<script src="index.js"></script>
+</html>
